d5bbb79dafeaec68b10e9f42736a1224cab47cc8
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-11-14 12:51:55 +0100

    add re-exports in __init__.py

--- src/yampit/__init__.py
+++ src/yampit/__init__.py
@@ -1,2 +1,4 @@
-def hello() -> str:
-    return "Hello from yampit!"
+from .polytope_request_handler import PolytopeRequestHandler
+from .mapper import Mapper
+
+__all__ = ["PolytopeRequestHandler", "Mapper"]
